b003d774da4b14b734f0d88811d637ee3db79fe0
yefeng <1226325909@qq.com>
2024-09-26 20:34:42 +0800

    perf: 优化

---
+++ webHookAction.py
@@ -0,0 +1,20 @@
+
+import requests
+
+
+HOME_ASSISTANT_URL = "http://ip:8123/api/webhook/"
+HEADERS = {
+    "Authorization": "xxxxx",
+}
+
+webHookAction = {
+    "切换卧室灯": "-_rZ-VozytptvKVGdnTwJ49v9",
+    "切换卧调大音量": "-_rZ-VozytptvKVGdnTwJ49v9"
+}
+
+def send_gesture(gesture):
+    #从webHookAction 中获取key 对应的value是webhook的id
+    webhook_id = webHookAction.get(gesture)
+    if webhook_id is None:
+        return
+    requests.post(HOME_ASSISTANT_URL+webhook_id, headers=HEADERS)
